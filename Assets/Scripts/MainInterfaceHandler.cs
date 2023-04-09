8f23709081b1a1579a3266de7b2712cfbec78856
murat-urmaz <muraturmaz@gmail.com>
2023-04-09 20:56:57 +0300

    Main menu system is fixed

---
+++ Assets/Scripts/MainInterfaceHandler.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainInterfaceHandler : MonoBehaviour
+{
+    public void DeactivateInterface(){
+        gameObject.SetActive(false);
+    }
+}
